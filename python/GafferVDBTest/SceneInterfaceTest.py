7d94219e5547257491af8a709d38bba261636786
Don Boogert <donbo@image-engine.com>
2017-09-08 16:28:06 -0700

    GafferVDB : basic SceneReader implementation

--- python/GafferVDBTest/SceneInterfaceTest.py
+++ python/GafferVDBTest/SceneInterfaceTest.py
@@ -3,17 +3,44 @@ import GafferTest
 import GafferVDB
 import IECore
 
+
 class SceneInterfaceTest( GafferTest.TestCase ) :
+	def setUp( self ) :
+		GafferTest.TestCase.setUp( self )
+		self.sceneInterface = IECore.SceneInterface.create( "/home/donbo/journal/data/vdb/utahteapot.vdb", IECore.IndexedIO.OpenMode.Read )
 
 	def testCanLoading( self ) :
-		sceneInterface = IECore.SceneInterface.create("/home/donbo/journal/data/vdb/utahteapot.vdb", IECore.IndexedIO.OpenMode.Read)
+		self.assertEqual( self.sceneInterface.hasObject(), True )
+		self.assertEqual( self.sceneInterface.fileName(), "/home/donbo/journal/data/vdb/utahteapot.vdb" )
+		self.assertEqual( self.sceneInterface.pathAsString(), "/" )
+		self.assertEqual( self.sceneInterface.name(), "" )
+		self.assertEqual( self.sceneInterface.hasBound(), True )
 
+	def testSingleVDBChildName( self ) :
+		self.assertEqual( self.sceneInterface.childNames(), ["vdb"] )
 
-	def testNoChildNames( self ):
-		sceneInterface = IECore.SceneInterface.create("/home/donbo/journal/data/vdb/utahteapot.vdb", IECore.IndexedIO.OpenMode.Read)
+	def testCanGetChildVDBLocation( self ):
+		self.assertEqual( self.sceneInterface.hasChild("vdb"), True)
+		childSceneInterface = self.sceneInterface.child("vdb")
+		self.assertEqual( childSceneInterface.childNames(), [])
 
-		self.assertEqual(sceneInterface.childNames(), [])
+	def testIdentityTransform( self ) :
+		transformData = self.sceneInterface.readTransform( 0.0 )
+		transform = self.sceneInterface.readTransformAsMatrix( 0.0 )
 
+		#todo check the transform
 
+	def testCanReadBounds( self ) :
+		bounds = self.sceneInterface.readBound( 0.0 )
 
+		# print type( bounds )
+		# print dir( bounds )
+		#
+		# print type( bounds.min )
+		# print dir( bounds.min )
 
+		# -50.055000305175781 -23.155000686645508 -30.854999542236328 48.055000305175781 23.155000686645508 30.854999542236328
+
+		# print transformData
+		# print transform
+		# todo check the bounds
--- src/GafferVDB/VDBScene.cpp
+++ src/GafferVDB/VDBScene.cpp
@@ -36,9 +36,12 @@
 #include "openvdb/openvdb.h"
 
 #include "IECore/SceneInterface.h"
-
+#include "IECore/SimpleTypedData.h"
 #include "GafferVDB/TypeIds.h"
 #include "GafferVDB/VDBObject.h"
+#include "GafferVDB/VDBUtil.h"
+
+#include "GafferScene/ScenePlug.h"
 
 using namespace Imath;
 using namespace IECore;
@@ -47,6 +50,8 @@ using namespace GafferVDB;
 using namespace openvdb;
 namespace
 {
+
+const SceneInterface::Name g_objectName("vdb");
 
 class VDBScene : public SceneInterface
 {
@@ -57,16 +62,9 @@ class VDBScene : public SceneInterface
 
 		VDBScene( const std::string &fileName, IndexedIO::OpenMode openMode )
 		: SceneInterface(),
-          m_fileName(fileName)
+			m_rootData( new RootData(fileName) )
+
 		{
-			// todo check if it's invalid to call this multiple times or is a performance hazard
-			openvdb::initialize();
-
-			io::File file( fileName );
-			file.open();
-
-			m_grids = file.readAllGridMetadata();
-			m_metaData = file.getMetadata();
 		}
 
 		~VDBScene() override
@@ -75,17 +73,34 @@ class VDBScene : public SceneInterface
 
 		std::string fileName() const override
 		{
-			return m_fileName;
+			return rootData().m_fileName;
 		}
 
 		Name name() const override
 		{
-			return Name("/");
+			if ( m_parent )
+			{
+				return g_objectName;
+			}
+			else
+			{
+				return Name();
+			}
+
 		}
 
 		void path( Path &p ) const override
 		{
-			p = Path();
+
+			if ( m_parent )
+			{
+				p = { g_objectName };
+			}
+			else
+			{
+				p = {};
+			}
+
 		}
 
 		////////////////////////////////////////////////////
@@ -94,7 +109,20 @@ class VDBScene : public SceneInterface
 
 		Imath::Box3d readBound( double time ) const override
 		{
-			return Box3d();
+			openvdb::GridPtrVecPtr grids = rootData().m_grids;
+
+			Imath::Box3d combinedBounds;
+			for ( auto gridIt = grids->begin(); gridIt != grids->end(); ++gridIt)
+			{
+				GridBase::Ptr gridPtr = *gridIt;
+				std::cout << "grid name: " << gridPtr->getName() << std::endl;
+
+
+				Imath::Box3d bounds = getBounds( gridPtr );
+				combinedBounds.extendBy(bounds);
+			}
+
+			return combinedBounds;
 		}
 
 		 void writeBound( const Imath::Box3d &bound, double time ) override
@@ -108,7 +136,7 @@ class VDBScene : public SceneInterface
 
 		ConstDataPtr readTransform( double time ) const override
 		{
-			return nullptr;
+			return new M44dData;
 		}
 
 		Imath::M44d readTransformAsMatrix( double time ) const override
@@ -176,8 +204,16 @@ class VDBScene : public SceneInterface
 
 		ConstObjectPtr readObject( double time ) const override
 		{
-			VDBObject::Ptr ptr = new VDBObject(m_grids->at(0));
-			return ptr;
+			if ( m_parent )
+			{
+				rootData().m_grids->at(0)->readNonresidentBuffers();
+				return  new VDBObject( rootData().m_grids->at(0) );
+			}
+			else
+			{
+				return nullptr;
+			}
+
 		}
 
 		PrimitiveVariableMap readObjectPrimitiveVariables( const std::vector<InternedString> &primVarNames, double time ) const override
@@ -197,13 +233,27 @@ class VDBScene : public SceneInterface
 		/// Convenience method to determine if a child exists
 		bool hasChild( const Name &name ) const override
 		{
+			if ( !m_parent && name == g_objectName)
+			{
+				return true;
+			}
+
 			return false;
 		}
 		/// Queries the names of any existing children of path() within
 		/// the scene.
 		void childNames( NameList &childNames ) const override
 		{
-			childNames = NameList();
+			if ( m_parent )
+			{
+				childNames = NameList();
+			}
+			else
+			{
+				childNames = { g_objectName };
+			}
+
+			std::cout << "childnames size: " << childNames.size() << std::endl;
 		}
 		/// Returns an object for the specified child location in the scene.
 		/// If the child does not exist then it will behave according to the
@@ -213,30 +263,41 @@ class VDBScene : public SceneInterface
 		/// to the parent as it is written.
 		SceneInterfacePtr child( const Name &name, MissingBehaviour missingBehaviour = ThrowIfMissing ) override
 		{
-			if (missingBehaviour == ThrowIfMissing)
+			if ( m_parent )
 			{
-				throw IECore::InvalidArgumentException("VDBSCene::child(): no child called \" + name.string()");
+				if (missingBehaviour == ThrowIfMissing)
+				{
+					throw IECore::InvalidArgumentException("VDBSCene::child(): no child called \" + name.string()");
+				}
+				else if (missingBehaviour == CreateIfMissing)
+				{
+					throw IECore::InvalidArgumentException("VDBScene::child(): CreateIfMissing not supported");
+				}
+				return nullptr;
 			}
-			else if (missingBehaviour == CreateIfMissing)
+			else
 			{
-				throw IECore::InvalidArgumentException("VDBScene::child(): CreateIfMissing not supported");
+				if (name == g_objectName)
+				{
+					return new VDBScene(this);
+				}
+				else if (missingBehaviour == ThrowIfMissing)
+				{
+					throw IECore::InvalidArgumentException("VDBSCene::child(): no child called \" + name.string()");
+				}
+				else if (missingBehaviour == CreateIfMissing)
+				{
+					throw IECore::InvalidArgumentException("VDBScene::child(): CreateIfMissing not supported");
+				}
 			}
+
 
 			return SceneInterfacePtr();
 		}
 		/// Returns a read-only interface for a child location in the scene.
 		ConstSceneInterfacePtr child( const Name &name, MissingBehaviour missingBehaviour = ThrowIfMissing ) const override
 		{
-			if (missingBehaviour == ThrowIfMissing)
-			{
-				throw IECore::InvalidArgumentException("VDBSCene::child(): no child called \" + name.string()");
-			}
-			else if (missingBehaviour == CreateIfMissing)
-			{
-				throw IECore::NotImplementedException("VDBScene::child(): CreateIfMissing not supported");
-			}
-
-			return SceneInterfacePtr();
+			return const_cast< VDBScene* >( this )->child( name, missingBehaviour );
 		}
 		/// Returns a writable interface to a new child. Throws an exception if it already exists.
 		/// Bounding boxes will be automatically propagated up from the children
@@ -248,12 +309,56 @@ class VDBScene : public SceneInterface
 		/// Returns a interface for querying the scene at the given path (full path).
 		SceneInterfacePtr scene( const Path &path, MissingBehaviour missingBehaviour = ThrowIfMissing ) override
 		{
-			throw IECore::InvalidArgumentException("");
+			if ( path.empty() )
+			{
+				if ( m_parent )
+				{
+					return m_parent;
+				}
+				else
+				{
+					return this;
+				}
+			}
+			else if ( path.size() == 1 && path[0] == g_objectName )
+			{
+				if ( m_parent )
+				{
+					return this;
+				}
+				else
+				{
+					return new VDBScene( this );
+				}
+			}
+			else
+			{
+				if (missingBehaviour == ThrowIfMissing)
+				{
+					std::string pathStr;
+					GafferScene::ScenePlug::pathToString(path, pathStr);
+					bool isRoot = (m_parent != nullptr);
+					if ( isRoot )
+					{
+						throw IECore::InvalidArgumentException("VDBSCene::scene(): no path called '" + pathStr + "' is root!" );
+					}
+					else
+					{
+						throw IECore::InvalidArgumentException("VDBSCene::scene(): no path called '" + pathStr + "'");
+					}
+
+				}
+				else if (missingBehaviour == CreateIfMissing)
+				{
+					throw IECore::NotImplementedException("VDBScene::scene(): CreateIfMissing not supported");
+				}
+				return nullptr;
+			}
 		}
 		/// Returns a const interface for querying the scene at the given path (full path).
 		ConstSceneInterfacePtr scene( const Path &path, MissingBehaviour missingBehaviour = ThrowIfMissing ) const override
 		{
-			throw IECore::InvalidArgumentException("");
+			return const_cast< VDBScene* >( this )->scene( path, missingBehaviour );
 		}
 
 		////////////////////////////////////////////////////
@@ -262,17 +367,65 @@ class VDBScene : public SceneInterface
 
 		void hash( HashType hashType, double time, MurmurHash &h ) const override
 		{
+			SceneInterface::hash (hashType, time, h);
+			h.append( hashType );
 
+			if ( hashType == ChildNamesHash )
+			{
+				h.append( m_parent == nullptr );
+			}
+			else if ( hashType == ObjectHash || hashType == BoundHash)
+			{
+				h.append( rootData().m_fileName );
+				h.append( m_parent == nullptr );
+			}
+			else if ( hashType == HierarchyHash )
+			{
+				h.append( m_parent == nullptr );
+			}
 		}
 
 	private :
 
+
+
+		VDBScene( VDBScene* parent )
+		: m_parent(parent)
+		{
+
+		}
+
 		static FileFormatDescription<VDBScene> g_description;
 
-		openvdb::GridPtrVecPtr m_grids;
-		openvdb::MetaMap::Ptr m_metaData;
-		std::string m_fileName;
+		struct RootData
+		{
+			RootData(const std::string& filename)
+			: m_fileName(filename)
+			{
+				// todo check if it's invalid to call this multiple times or is a performance hazard
+				openvdb::initialize();
 
+				io::File file( m_fileName );
+				file.open();
+
+				m_grids = file.getGrids();
+				m_metaData = file.getMetadata();
+			}
+
+			openvdb::GridPtrVecPtr m_grids;
+			openvdb::MetaMap::Ptr m_metaData;
+			std::string m_fileName;
+		};
+
+		RootData &rootData() const
+		{
+			if ( m_parent )
+				return m_parent->rootData();
+			else
+				return *m_rootData;
+		}
+
+		std::shared_ptr<RootData> m_rootData;
 		Ptr m_parent;
 
 };
