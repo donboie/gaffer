cc0c93e6fd9074cce8f02ee16f11dbc2ecb7c61d
Don Boogert <donbo@image-engine.com>
2018-02-26 10:44:11 -0800

    travis test

--- config/travis/installDependencies.py
+++ config/travis/installDependencies.py
@@ -2,6 +2,7 @@ import os
 import re
 import sys
 import urllib
+import ssl
 
 # figure out where we'll be making the build
 
@@ -15,10 +16,23 @@ buildDir = "build/gaffer-%d.%d.%d.%d-%s" % ( gafferMilestoneVersion, gafferMajor
 
 # get the prebuilt dependencies package and unpack it into the build directory
 
+if platform == 'osx':
+	context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+else:
+	context = None
+
 downloadURL = "https://github.com/GafferHQ/dependencies/releases/download/0.44.0.0/gafferDependencies-0.44.0.0-" + platform + ".tar.gz"
 
 sys.stderr.write( "Downloading dependencies \"%s\"" % downloadURL )
-tarFileName, headers = urllib.urlretrieve( downloadURL )
+
+if platform == 'osx':
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+	tarFileName, headers = urllib.urlretrieve( downloadURL, context = context)
+else:
+        context = None
+	tarFileName, headers = urllib.urlretrieve( downloadURL)
+
+
 
 os.makedirs( buildDir )
 os.system( "tar xf %s -C %s --strip-components=1" % ( tarFileName, buildDir ) )
